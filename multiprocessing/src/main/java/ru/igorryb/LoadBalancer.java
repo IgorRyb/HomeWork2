ed9c046abdf8d7cdd5edc575f23c78378179c4d2
IgorRyb <123228100+IgorRyb@users.noreply.github.com>
2024-02-21 19:43:57 +0500

    correct project

--- multiprocessing/src/main/java/ru/igorryb/LoadBalancer.java
+++ multiprocessing/src/main/java/ru/igorryb/LoadBalancer.java
@@ -1,59 +1,92 @@
 package ru.igorryb;
 
 import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class LoadBalancer extends Thread {
+public class LoadBalancer {
 
-    private List<Node> nodes;
-    private int index;
+    private static List<Node> nodes = Collections.synchronizedList(new ArrayList<>());
 
-    public LoadBalancer() {
-        index = 0;
-        this.nodes = Collections.synchronizedList(new ArrayList<>());
-        start();
+    private static final int LISTENING_PORT = 8989;
+    private static final int REQUESTING_PORT = 9091;
+
+    public static void main(String[] args) {
+        nodeListener();
+        responseProcess();
     }
 
-    @Override
-    public void run() {
-        while (true) {
-            sleep();
-            Node node = getNextNode();
-            System.out.println("_____");
-            try (Socket socket = new Socket("localhost", node.getPort());
-                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                 PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
-                String message = bufferedReader.readLine();
-                if (message.equals("check")) {
-                    registerNode(new Node(socket.getPort(), this));
+    public static void nodeListener() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    sleep();
+                    try (ServerSocket serverSocket = new ServerSocket(LISTENING_PORT)) {
+                        Socket socket = serverSocket.accept();
+                        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                             PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+                            int raisedNode = Integer.parseInt(bufferedReader.readLine());
+                            Node regitsteredNode = registerNode(new Node(raisedNode, socket.getInetAddress().getHostAddress()));
+                            printWriter.println("Node registered: " + regitsteredNode);
+                        }
+                    } catch (IOException e) {}
                 }
-                String msg = "processed";
-                printWriter.println(msg + "\n");
-                int port = Integer.parseInt(bufferedReader.readLine());
-                System.out.println("Node with port: " + port + " registered");
-            } catch (IOException e) {}
-        }
+            }
+        }).start();
     }
 
+    public static void responseProcess() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try (ServerSocket serverSocket = new ServerSocket(REQUESTING_PORT)) {
+                        Socket socket = serverSocket.accept();
+                        try (PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true);
+                             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                            String msg = bufferedReader.readLine();
+                            if (nodes.size() != 0) {
+                                String result = sendMessage(msg);
+                                printWriter.println(result);
+                            } else {
+                                throw new RuntimeException("Nodes is empty!");
+                            }
+                        }
+                    } catch (IOException e) {}
+                }
+            }
+        }).start();
+    }
 
-    public void registerNode(Node node) {
+    private static String sendMessage(String msg) {
+        int randomNumber = getRandomNumber();
+        Node node = nodes.get(randomNumber);
+        String nodeMessage = null;
+        try (Socket socket = new Socket(node.getHost(), node.getPort());
+            PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            printWriter.println(msg);
+            nodeMessage = bufferedReader.readLine();
+        } catch (IOException e) {}
+        return nodeMessage;
+    }
+
+    private static int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(nodes.size());
+    }
+
+    private static Node registerNode(Node node) {
         nodes.add(node);
-    }
-
-    public Node getNextNode() {
-        Node node = nodes.get(index);
-        if (index != nodes.size() - 1) {
-            index++;
-        } else {
-            index = 0;
-        }
         return node;
     }
 
-    private void sleep() {
+    private static void sleep() {
         try {
             Thread.sleep(500);
         } catch (InterruptedException e) {
--- multiprocessing/src/main/java/ru/igorryb/Node.java
+++ multiprocessing/src/main/java/ru/igorryb/Node.java
@@ -4,39 +4,70 @@ package ru.igorryb;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
-public class Node extends Thread {
+public class Node {
 
-    private int port;
-    private LoadBalancer loadBalancer;
+    private static int port;
+    private static String host;
+    private static final int RESPONSE_PORT = 8989;
+    private static final Scanner scanner = new Scanner(System.in);
 
-    public Node(int port, LoadBalancer loadBalancer) {
-        this.port = port;
-        this.loadBalancer = loadBalancer;
-        loadBalancer.registerNode(this);
-        start();
+    public static void main(String[] args) {
+        int registeredPort = sendSignal();
+        handle(registeredPort);
     }
 
-    @Override
-    public void run() {
+    public Node(int port, String host) {
+        this.port = port;
+        this.host = host;
+    }
+
+    private static int sendSignal() {
+        int port = scanner.nextInt();
+        try (Socket socket = new Socket("localhost", RESPONSE_PORT);
+                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
+            printWriter.println(port);
+            System.out.println(bufferedReader.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return port;
+    }
+
+    private static void handle(int port) {
         while (true) {
             try (ServerSocket serverSocket = new ServerSocket(port);
                  Socket socket = serverSocket.accept();
                  BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                  PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()), true)) {
-                printWriter.println("check");
-                printWriter.flush();
                 String msg = bufferedReader.readLine();
-                printWriter.println(port);
-            } catch (IOException e) {}
+                printWriter.println("ready for processing");
+                if (!msg.equals(null)) {
+                    printWriter.println(msg);
+                }
+            } catch (IOException e) {
+
+            }
         }
     }
 
-    public int getPort() {
+    public static int getPort() {
         return port;
     }
 
-    public void setPort(int port) {
-        this.port = port;
+    public static void setPort(int port) {
+        Node.port = port;
+    }
+
+    public static String getHost() {
+        return host;
+    }
+
+    public static void setHost(String host) {
+        Node.host = host;
     }
 }
